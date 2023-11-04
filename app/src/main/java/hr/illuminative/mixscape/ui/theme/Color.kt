0b5b2555065d6e43446f3a5a691b0fd1ccd72ab6
Noa Midzic <nmidzic@yahoo.com>
2023-11-04 16:43:35 +0100

    Added composable components for screens

--- app/src/main/java/hr/illuminative/mixscape/ui/theme/Color.kt
+++ app/src/main/java/hr/illuminative/mixscape/ui/theme/Color.kt
@@ -4,3 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val DarkBlue = Color(0xff004aad)
 val LightBlue = Color(0xffbbd6ed)
+
+val Gray700 = Color(0xFF818181)
+val DarkGreen = Color(0xFF04452e)
+
